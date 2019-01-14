86258ddb2c6be4e49d0e1fce957e0b8236b7d982
Andrew <andrewpeliza@hotmail.com>
2019-01-14 16:33:50 +0000

    Frontend and backend are now connected and working as expected.

--- src/containers/Admin/Admin.js
+++ src/containers/Admin/Admin.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 // Redirect,
 // import { connect } from 'react-redux';
 
 import Aux from '../../hoc/Aux';
 import './Admin.css';
+import Article from '../../components/Article/Article';
 
 class Admin extends Component {
 
@@ -24,11 +25,20 @@ class Admin extends Component {
     }
 
     render() {
-        return (
-            <Aux>
-                <h1>Test Admin</h1>
-            </Aux>
-        );
+        const Articles = this.state.articles.map(article => {
+            return  (
+            <Article
+              key={article.title}
+              title={article.title}
+              content={article.content} />
+            );
+          });
+            return (
+                <Aux>
+                    <h1>Test Admin</h1>
+                    {Articles}
+                </Aux>
+            );
     }
 }
 
