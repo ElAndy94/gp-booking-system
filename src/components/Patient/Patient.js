d9cf9fcf6db2789c8ceb1be14d37125679915f0d
Andrew <andrewpeliza@hotmail.com>
2019-01-16 17:43:25 +0000

    Adding data and style to the admin and patients page.

--- src/components/Patient/Patient.js
+++ src/components/Patient/Patient.js
@@ -1,8 +1,30 @@
 import React from "react";
 
+import './Patient.css';
+
 const patient = (props) => (
-  <article>
-        <h2>{props.title + ' ' + props.firstName + ' ' + props.lastName}</h2>
-    </article>
+    <div className="container">
+        <div className="card">
+            <header className="card-header">
+                <p className="card-header-title">
+                    {props.title + ' ' + props.firstName + ' ' + props.lastName}
+                </p>
+            </header>
+            <div className="card-content">
+                <div className="content">
+                Remind me to thank John for a lovely weekend. So you two dig up, dig up dinosaurs.
+                <br />
+                <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+            </div>
+            <footer className="card-footer">
+                <a href="/" className="card-footer-item">Save</a>
+                <a href="/" className="card-footer-item">Edit</a>
+                <a href="/" className="card-footer-item">Delete</a>
+            </footer>
+        </div>
+        <br />
+    </div>
 );
+
 export default patient;
--- src/containers/Patients/Patients.js
+++ src/containers/Patients/Patients.js
@@ -5,12 +5,47 @@ import { withRouter } from 'react-router-dom';
 
 import Aux from '../../hoc/Aux';
 import './Patients.css';
+import Patient from '../../components/Patient/Patient';
 
 class Patients extends Component {
+    state = {
+        patients:[
+            {
+                title: 'Mr',
+                firstName: 'Andrew',
+                lastName: 'Peliza'
+            }, 
+            {
+                title: 'Mr',
+                firstName: 'Charles',
+                lastName: 'Nico'
+            }
+        ]
+    }
+
+    // componentDidMount() {
+    //     axios.get('http://127.0.0.1:8000/api/')
+    //         .then(res => {
+    //             this.setState({
+    //                 articles: res.data
+    //             });
+    //         })
+    // }
+
     render() {
+        const Patients = this.state.patients.map(patient => {
+            return  (
+            <Patient
+              key={patient.lastName}
+              title={patient.title}
+              firstName={patient.firstName}
+              lastName={patient.lastName} />
+            );
+          });
         return(
             <Aux>
                 <h1>Test Patients</h1>
+                {Patients}
             </Aux>
         );
     }
